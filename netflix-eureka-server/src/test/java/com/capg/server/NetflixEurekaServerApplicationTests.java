f06d6697cf9ec02debe57ac1fd2b5898eb674cab
ramanujdas <ramanujds9@gmail.com>
2020-04-11 09:53:07 +0530

    Eureka Server

---
+++ netflix-eureka-server/src/test/java/com/capg/server/NetflixEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
